576e28a47420ef28678be61c4f040741d5461bbf
Nelson M <nelsontron@outlook.com>
2020-06-10 20:00:50 -0600

    changed danger weight value

--- src/snake.py
+++ src/snake.py
@@ -83,7 +83,7 @@ class Snake:
 
     # WEIGHING LOGIC CALLED IN weigh() FUNCTION
     def weigh_dangers(self, direction):
-        VALUE = 100
+        VALUE = 999
         future = Move.future_point(direction, self.head)
         return -VALUE if future in self.dangers else VALUE
 
